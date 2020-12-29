f9e9932f9ce3ed0225fe09e6f7ad5aa34e68280a
Kinji_Kamizaki <Kinji.Kamizaki@gmail.com>
2020-12-29 16:01:52 +0900

    first commit

---
+++ setVolumePermission.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+USER_ID=${UID:-9001}
+GROUP_ID=${GID:-9001}
+
+useradd -u $USER_ID -o -m user
+groupmod -g $GROUP_ID user
+export HOME=/home/user
+chown -R user /opt/LR
+busybox crond -f -L /dev/stderr
+
+exec gosu user:user bash
